8b600ea78d732675bee0d79bea5a0a0850de0274
Adam Blevins <blevinspercussion@gmail.com>
2022-07-16 19:04:20 -0500

    finished mobile styling

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -7,31 +7,31 @@ function Graph () {
     return (
         <div className='graph'>
             <div className='data'>
-                <div id='rect1'></div>
+                <div className='rect' id='rect1'></div>
                 <p>mon</p>
             </div>
             <div className='data'>
-                <div id='rect2'></div>
+                <div className='rect' id='rect2'></div>
                 <p>tue</p>
             </div>
             <div className='data'>
-                <div id='rect3'></div>
+                <div className='rect' id='rect3'></div>
                 <p>wed</p>
             </div>
             <div className='data'>
-                <div id='rect4'></div>
+                <div className='rect' id='rect4'></div>
                 <p>thu</p>
             </div>
             <div className='data'>
-                <div id='rect5'></div>
+                <div className='rect' id='rect5'></div>
                 <p>fri</p>
             </div>
             <div className='data'>
-                <div id='rect6'></div>
+                <div className='rect' id='rect6'></div>
                 <p>sat</p>
             </div>
             <div className='data'>
-                <div id='rect7'></div>
+                <div className='rect' id='rect7'></div>
                 <p>sun</p>
             </div>
 
